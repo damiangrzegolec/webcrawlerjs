088a56419f3fde0f29983d8b34ae1e6a5248c18b
Damian Grzegolec <contact@damiangrzegolec.pl>
2023-09-30 15:40:15 +0200

    crawl page recursive

--- crawl.js
+++ crawl.js
@@ -1,23 +1,45 @@
 const {JSDOM} = require('jsdom')
 
-async function crawlPage(curretnURL) {
-    console.log(`actively crawling: ${curretnURL}`)
+async function crawlPage(baseURL, currentURL, pages) {
+    const baseURLObj = new URL(baseURL)
+    const currentURLObj = new URL(currentURL)
+
+    if(baseURLObj.hostname !== currentURLObj.hostname) {
+        return pages
+    }
+
+    const normalizedCurrentURL = normalizeURL(currentURL)
+    if(pages[normalizedCurrentURL] > 0) {
+        pages[normalizedCurrentURL]++
+        return pages
+    }
+
+    pages[normalizedCurrentURL] = 1
+    console.log(`actively crawling: ${currentURL}`)
+
     try {
-        const resp = await fetch(curretnURL)
+        const resp = await fetch(currentURL)
         if(resp.status > 399) {
-            console.log(`error in fetch with status code: ${resp.status} on page: ${curretnURL}`)
-            return
+            console.log(`error in fetch with status code: ${resp.status} on page: ${currentURL}`)
+            return pages
         }
         const contentType = resp.headers.get("content-type")
         if(!contentType.includes("text/html")) {
-            console.log(`no html resposne, content type: ${contentType} on page: ${curretnURL}`)
-            return
+            console.log(`no html resposne, content type: ${contentType} on page: ${currentURL}`)
+            return pages
         }
 
-        console.log(await resp.text())
+        const htmlBody = await resp.text()
+
+        const nextUrls = getURLsFromHTML(htmlBody, baseURL)
+
+        for(const nextURL of nextUrls) {
+            pages = await crawlPage(baseURL, nextURL, pages)
+        }
     } catch (err) {
-        console.log(`err.message on page: ${curretnURL}`)
+        console.log(`err.message on page: ${currentURL}`)
     }
+    return pages
 }
 
 function getURLsFromHTML(htmlBody, baseURL) {
